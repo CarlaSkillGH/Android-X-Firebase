6174e3678f0c5b52398c91df7d821bd864a2966d
Victor Alzate <alzatebalzan@gmail.com>
2024-10-14 14:52:52 -0500

    second commit

--- app/src/main/java/com/example/androidxfirebase/CreateEmpleadoActivity.java
+++ app/src/main/java/com/example/androidxfirebase/CreateEmpleadoActivity.java
@@ -1,14 +1,28 @@
 package com.example.androidxfirebase;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androidxfirebase.Data.adapter.EmpleadoAdapter;
+import com.example.androidxfirebase.Data.dao.EmpleadoDao;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CreateEmpleadoActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+    private EmpleadoDao empleadoDao;
+    private EditText editTextEmpleadoname,editTextEmail;
+    private EmpleadoAdapter empleadoAdapter;
+    private Button regresar,crear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,28 @@ public class CreateEmpleadoActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        db = FirebaseFirestore.getInstance();
+        empleadoDao = new EmpleadoDao(db);
+
+        regresar = findViewById(R.id.buttonReturnU);
+        crear = findViewById(R.id.buttonCreateEmpleado);
+
+        regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Cierra la actividad actual
+            }
+        });
+
+        crear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTextEmpleadoname.getText().toString();
+                String email = editTextEmail.getText().toString();
+                
+            }
         });
     }
 }
